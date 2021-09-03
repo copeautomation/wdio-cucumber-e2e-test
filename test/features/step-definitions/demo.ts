e3b5a529573abe1342db0976038958e208419fb7
copeautomation <copeautomation@gmail.com>
2021-09-03 16:22:33 +1000

    web table part 1

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -30,7 +30,7 @@ Then(/^URL should match (.*)$/, async function (expectedURL) {
  * Web Interactions
  */
 Given(/^A web page is opened$/, async function () {
-    await browser.url("/")
+    await browser.url("https://the-internet.herokuapp.com/tables")
     await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
     // await browser.maximizeWindow()
 })
@@ -195,7 +195,54 @@ When(/^Perfom web interactions$/, async function () {
      * 1. scrollIntoView
      * 
      */
-    await $('span=Best Sellers in Books').scrollIntoView()
+    // await $('span=Best Sellers in Books').scrollIntoView()
+
+    /** 
+     * Web table:
+     * What are going to cover:
+     * 1. Check number of rows and columns
+     * 2. Get whole table data
+     * 3. Get single row [based on a condition]
+     * 4. Get single column 
+     * 5. Get single cell value [based on another cell]
+     */
+    /**1. Check number of rows and columns */
+   let rowCount = await $$(`//table[@id="table1"]/tbody/tr`).length
+   chai.expect(rowCount).to.equal(4)
+   console.log(`>> Number of rows: ${rowCount}`);
+   let colCount = await $$(`//table[@id="table1"]/thead/tr/th`).length
+   console.log(`>> Number of cols: ${colCount}`);
+   chai.expect(colCount).to.equal(6)
+
+    /**2. Get whole table data */
+    let arr =  []
+    for (let i = 0; i < rowCount; i++) {
+        let personObj = {
+            lastname: "",
+            firstname : "",
+            email: "",
+            due: "",
+            web: "",
+        }
+        for (let j = 0; j < colCount; j++) {
+            let cellVal = await $(`//table[@id="table1"]/tbody/tr[${i + 1}]/td[${j + 1}]`).getText()
+            if (j === 0) personObj.lastname = cellVal
+            if (j === 1) personObj.firstname = cellVal
+            if (j === 2) personObj.email = cellVal
+            if (j === 3) personObj.due = cellVal
+            if (j === 4) personObj.web = cellVal
+        }
+        arr.push(personObj)
+
+    }
+    console.log(`Whole table: ${JSON.stringify(arr)}`);
+
+    /**3. Get single row [based on a condition] */
+
+    /**4. Get single column */
+
+    /**5. Get single cell value [based on another cell] */
+
 
     await browser.debug()
 
