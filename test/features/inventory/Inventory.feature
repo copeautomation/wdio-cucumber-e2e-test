dc04e0fa281b46a80d9a0c2ff5e7ad1a402ee370
copeautomation <copeautomation@gmail.com>
2021-09-08 15:33:00 +1000

    add data table example

--- test/features/inventory/Inventory.feature
+++ test/features/inventory/Inventory.feature
@@ -1,7 +1,12 @@
 Feature: Inventory
+
     @demo @smoke @debug
     Scenario Outline: Demo Inventory
         Given As a standard user I login to inventory web app
+            | UserType | Username                |
+            | StdUser  | standard_user           |
+            | ProbUser | problem_user            |
+            | PerfUser | performance_glitch_user |
         Then Inventory page should list <NumberOfProducts>
         Then Validate all products have valid price
 
--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -1,7 +1,13 @@
 import { Given } from "@cucumber/cucumber";
 import chai from "chai";
 
-Given(/^As (a|an) (.*) user I login to inventory web app$/, async function (prefixTxt, userType) {
+Given(/^As (a|an) (.*) user I login to inventory web app$/, async function (prefixTxt, userType, dataTable) {
+    // Getting values from data table
+    let dt = dataTable.hashes()
+    console.log(`>> The type of dt: ${typeof dt}`); // object
+    console.log(`>> The type of dt: ${dt.constructor}`); 
+    console.log(`>> The value of dt: ${JSON.stringify(dt)}`);
+
     console.log(`>> The userType: ${userType}`);
     /** 1. Launch browser */
     // @ts-ignore
@@ -11,7 +17,8 @@ Given(/^As (a|an) (.*) user I login to inventory web app$/, async function (pref
 
     /**2. Login to inventory*/
     try {
-        await $(`#user-name`).setValue(process.env.TEST_STD_USERNAME)
+        await $(`#user-name`).setValue(dt[0].Username)
+        // await $(`#user-name`).setValue(process.env.TEST_STD_USERNAME)
         await $(`#password`).setValue(process.env.TEST_STD_PASSWORD)
         await $(`#login-button`).click()
     } catch (err) {
@@ -22,7 +29,7 @@ Given(/^As (a|an) (.*) user I login to inventory web app$/, async function (pref
         await $(`#password`).setValue("secret_sauce")
         await $(`#login-button`).click()
     }
-    
+
     /** Login with another */
     // await browser.pause(2000)
     // await browser.reloadSession()
@@ -35,5 +42,5 @@ Given(/^As (a|an) (.*) user I login to inventory web app$/, async function (pref
     // await browser.pause(2000)
     // await  browser.forward()
     // await browser.debug()
-    
+
 })
