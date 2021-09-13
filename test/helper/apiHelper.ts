ed03b5a4f2b5ab6c8c41a598cff1bbd44047cc40
copeautomation <copeautomation@gmail.com>
2021-09-13 21:18:00 +1000

    add get method

--- test/helper/apiHelper.ts
+++ test/helper/apiHelper.ts
@@ -1,6 +1,30 @@
 import request from "supertest"
+import reporter from "../helper/reporter"
 
-// What is request
-console.log(`> The typeof request: ${typeof request}`); //function
-console.log(`>> Number of args: ${request.length}`); // 1 arg
-console.log(`>> what is the definition of the function: ${request.toString()}`);
+async function GET(testid: string, baseURL: string, endpoint: string, authToken: string, queryParam: object,) {
+    if (!baseURL || !endpoint) {
+        throw Error(`One of the given values baseURL: ${baseURL}, endpoint: ${endpoint} is not valid `)
+    }
+    baseURL = baseURL.trim()
+    endpoint = endpoint.trim()
+    reporter.addStep(testid, "info", `Making a GET to ${endpoint}`)
+    try {
+        return await request(baseURL)
+            .get(endpoint)
+            .query(queryParam)
+            .auth(authToken, { type: 'bearer' })
+            .set("Content-Type", "application/json")
+            .set("Accept", "application/json")
+    } catch (err) {
+        err.message = `Error making a GET call to ${endpoint}, ${err}`
+        throw err
+    }
+}
+
+export default { GET }
+/**
+ * https://reqres.in
+ * /api/users?page=2
+ * {pages: 2}
+ *
+ */
