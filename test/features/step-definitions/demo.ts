bab8c6d3090b418231a50fb27db59e5dbcaf47f8
copeautomation <copeautomation@gmail.com>
2021-11-20 12:53:55 +1100

    update logger for demo test

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,19 +1,23 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import chai from "chai"
+import reporter from "../../helper/reporter"
 
 
 Given(/^Google page is opened$/, async function () {
+    reporter.addStep(this.testid, "info", "Opening Google home page...")
     await browser.url("https://www.google.com")
     await browser.pause(1000)
 })
 
 When(/^Search with (.*)$/, async function (searchItem) {
+    reporter.addStep(this.testid, "info", `Searching: ${searchItem} in Google...`)
     let ele = await $(`[name=q]`)
     await ele.setValue(searchItem)
     await browser.keys("Enter")
 })
 
 Then(/^Click on the first search result$/, async function () {
+    reporter.addStep(this.testid, "info", `Clicking on first search items...`)
     let ele = await $(`<h3>`)
     ele.click()
 })
